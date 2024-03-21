1ed083b09d0c3999d1d9ccfc9b23502b17fe5ebc
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-03-21 13:02:59 -0300

    .

--- src/componentes/banner/index.jsx
+++ src/componentes/banner/index.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+import mobile from '../../../public/images/bg-main-mobile.png'
+
+import desktop from '../../../public/images/bg-main-desktop.png'
+
 const ContainerBanner = styled.div`
-    background-image: url('/public/images/bg-main-desktop.png');
+    background-image: url(${desktop});
     height: 100vh;
     width: 30vw;
 
     @media only screen and (max-width: 600px) {
-        background-image: url('/public/images/bg-main-mobile.png');
+        background-image: url(${mobile});
         width: 100vw;
       
     }
