b6fbd3bcf4a05bf646837beaadecab2e644cdaa5
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-03-21 12:59:51 -0300

    .

--- src/componentes/banner/index.jsx
+++ src/componentes/banner/index.jsx
@@ -6,40 +6,19 @@ const ContainerBanner = styled.div`
     height: 100vh;
     width: 30vw;
 
-    img{
-        width: 30vw;
-        height: 100%;
-    }
-
     @media only screen and (max-width: 600px) {
         background-image: url('/public/images/bg-main-mobile.png');
         width: 100vw;
-
-
-        img{
-        width: 100vw;
-        height: 70%;
-    }
+      
     }
 `
 
 const Banner = () =>{
-    const [banner, setBanner] = useState('');
-
-    useEffect(() => {
-        // Verificar se é um dispositivo móvel
-        const isMobile = window.matchMedia("(max-width: 600px)").matches;
-        // Definir o banner com base no dispositivo
-        if (isMobile) {
-            setBanner('/public/images/bg-main-mobile.png');
-        } else {
-            setBanner('/public/images/bg-main-desktop.png');
-        }
-    }, []);
+    
 
     return(
         <ContainerBanner>
-            <img src={banner} alt="Banner" />
+            
         </ContainerBanner>
     )
 }
